e0c7dfc7317d325e268f02030856c0444f4c41d4
nicolasvelazquez25 <nicolas.velazquez@correo.ucu.edu.uy>
2023-10-30 09:31:07 -0300

    Cambio el SecurityConfig
    Cambio a otro tipo para que no este deprecado, sigue dando 401 cualquier request igualmente

--- backend/src/main/java/los/trainees/backend/config/SecurityConfig.java
+++ backend/src/main/java/los/trainees/backend/config/SecurityConfig.java
@@ -2,10 +2,14 @@ package los.trainees.backend.config;
 
 import los.trainees.backend.config.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -13,15 +17,19 @@ public class SecurityConfig {
 
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
-    
-    protected void configure(HttpSecurity http) throws Exception {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/category/**").permitAll()
-                .antMatchers("/provider/**").hasRole("ADMIN")
-                .antMatchers("/provider/**").hasRole("PROVIDER")
-                .and()
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/login/**").permitAll()
+                        .requestMatchers("/category/**").permitAll()
+                        .requestMatchers("/provider/**").hasRole("ADMIN")
+                        .requestMatchers("/provider/**").hasRole("PROVIDER")
+                )
+                .addFilterAfter(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .httpBasic(withDefaults());
+
+        return http.build();
     }
 }

c59c52fd6aadc3cfad5d2fa8b16a2cc8ddd7a32c
jeronimo-espiga <jeronimo.espiga@correo.ucu.edu.uy>
2023-10-31 20:49:10 -0300

    Changes

--- backend/src/main/java/los/trainees/backend/controller/UserController.java
+++ backend/src/main/java/los/trainees/backend/controller/UserController.java
@@ -1,19 +1,15 @@
 package los.trainees.backend.controller;
 
 import los.trainees.backend.dto.RUser;
-import los.trainees.backend.entity.User;
-import los.trainees.backend.enums.ERole;
 import los.trainees.backend.mapper.UserMapper;
-import los.trainees.backend.service.UserService;
 import los.trainees.backend.service.UserService;
 import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -24,7 +20,7 @@ public class UserController {
     private final UserMapper userMapper = Mappers.getMapper(UserMapper.class);
 
     @GetMapping(path = "/profile", produces = "application/json")
-    public User profile(@RequestParam id){
+    public RUser profile(@RequestParam Long id){
         return userMapper.toDto(userService.filter(id));
     }
 }
--- backend/src/main/java/los/trainees/backend/mapper/UserMapper.java
+++ backend/src/main/java/los/trainees/backend/mapper/UserMapper.java
@@ -4,8 +4,10 @@ import los.trainees.backend.dto.RUser;
 import los.trainees.backend.entity.User;
 import org.mapstruct.Mapper;
 
+import java.util.Optional;
+
 @Mapper
 public interface UserMapper {
 
-    RUser toDto(User user);
+    RUser toDto(Optional<User> user);
 }

b1e4913b8db9f2d5dcda44d52235528682e53c01
LautaroDaRosa <lautaro.darosa@correo.ucu.edu.uy>
2023-11-05 22:44:50 -0300

    feature/62 Make an endpoint to create or delete current provider's logged in association with a category

--- backend/src/main/java/los/trainees/backend/entity/ProviderCategory.java
+++ backend/src/main/java/los/trainees/backend/entity/ProviderCategory.java
@@ -1,10 +1,19 @@
 package los.trainees.backend.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 import los.trainees.backend.enums.ECategory;
 
+import java.io.Serializable;
+
 @Entity
-public class ProviderCategory {
+@IdClass(ProviderCategory.class)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProviderCategory implements Serializable {
 
     @Id
     @ManyToOne

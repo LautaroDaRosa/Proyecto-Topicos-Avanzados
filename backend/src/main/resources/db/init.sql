54d1ebea83d2c4ae2f34cf0722133d88cabae351
LautaroDaRosa <lautaro.darosa@correo.ucu.edu.uy>
2023-11-10 22:05:14 -0300

    Fix score filter for providers, added logo to user, fixed init.sql

--- backend/src/main/resources/db/init.sql
+++ backend/src/main/resources/db/init.sql
@@ -1,21 +1,30 @@
-INSERT IGNORE INTO user (user_id, email, info, username, password, phone, role)
-VALUES (1, 'provider1@example.com', 'Provider 1 Info', 'Provider 1', '48690', '48690', 'PROVIDER'),
-       (2, 'provider2@example.com', 'Provider 2 Info', 'Provider 2', '48690', '48690', 'PROVIDER'),
-       (3, 'provider3@example.com', 'Provider 3 Info', 'Provider 3', '48690', '48690', 'PROVIDER'),
-       (4, 'provider4@example.com', 'Provider 4 Info', 'Provider 4', '48690', '48691', 'PROVIDER'),
-       (5, 'topicos@example.com', 'User Test', 'topicos', '48690', '48691', 'PARTNER'),
-       (6, 'admin@example.com', 'User Admin', 'admin', '48690', '48691', 'ADMIN');
-INSERT IGNORE INTO provider (address, business_name, contact, logo, rut,user_id)
-VALUES ('Lorenzo Carnelli 1234', 'CANAL 10 S.A.', '',
+INSERT IGNORE INTO user (user_id, email, info, logo, username, password, phone, role)
+VALUES (1, 'provider1@example.com', 'Provider 1 Info',
         'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Canal_10_Uruguay_Logo_1960.webp/640px-Canal_10_Uruguay_Logo_1960.webp.png',
-        '734879753738964', 1),
-       ('Paraguay, 2253, Montevideo, Uruguay', 'CANAL 4 S.A.', '',
-        'https://upload.wikimedia.org/wikipedia/commons/3/33/Canal4_uy.png', '633334234363643', 2),
-       ('Enriqueta Compte y Riqué 1276', 'TELEDOCE S.A.', '',
-        'https://upload.wikimedia.org/wikipedia/commons/7/79/Teledoce_2022.png', '3457385937529', 3),
-       ('18 de Julio 1321', 'SANTANDER S.A.', '',
+        'Provider 1', '48690', '48690', 'PROVIDER'),
+       (2, 'provider2@example.com', 'Provider 2 Info',
+        'https://upload.wikimedia.org/wikipedia/commons/3/33/Canal4_uy.png', 'Provider 2', '48690', '48690',
+        'PROVIDER'),
+       (3, 'provider3@example.com', 'Provider 3 Info',
+        'https://upload.wikimedia.org/wikipedia/commons/7/79/Teledoce_2022.png', 'Provider 3', '48690', '48690',
+        'PROVIDER'),
+       (4, 'provider4@example.com', 'Provider 4 Info',
         'https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Banco_santander_logo.svg/2560px-Banco_santander_logo.svg.png',
-        'RUT1', 4);
+        'Provider 4', '48690', '48691', 'PROVIDER'),
+       (5, 'partner@example.com', 'User Test', 'https://deres.org.uy/wp-content/uploads/2020/02/deres_logo_celeste.png',
+        'partner', '48690', '48691', 'PARTNER'),
+       (6, 'admin@example.com', 'User Admin', 'https://deres.org.uy/wp-content/uploads/2020/02/deres_logo_celeste.png',
+        'admin', '48690', '48691', 'ADMIN');
+
+INSERT IGNORE INTO provider (address, business_name, contact, rut, user_id)
+VALUES ('Lorenzo Carnelli 1234', 'CANAL 10 S.A.', 'contact',
+        '734879753738964', 1),
+       ('Paraguay, 2253, Montevideo, Uruguay', 'CANAL 4 S.A.', 'contact',
+        '633334234363643', 2),
+       ('Enriqueta Compte y Riqué 1276', 'TELEDOCE S.A.', 'contact',
+        '3457385937529', 3),
+       ('18 de Julio 1321', 'SANTANDER S.A.', 'contact', 'RUT1', 4);
+
 INSERT IGNORE INTO provider_category (category, provider_user_id)
 VALUES ('AGRO_FORESTAL_OTROS', 1),
        ('COMERCIO_RETAIL_SERVICIOS', 1),
@@ -27,8 +36,15 @@ VALUES ('AGRO_FORESTAL_OTROS', 1),
        ('SERVICIO_FINANCIERO_AFINES', 4),
        ('SERVICIO_GASTRONOMICO_AFINES', 3),
        ('SERVICIO_GASTRONOMICO_AFINES', 1);
-INSERT IGNORE INTO partner (address, business_name, contact, logo, rut, user_id)
-VALUES ('address', 'business_name', 'contact', 'logo', 'rut', 5);
+
+INSERT IGNORE INTO partner (address, business_name, contact, rut, user_id)
+VALUES ('address', 'business_name', 'contact', 'rut', 5);
 
 INSERT IGNORE INTO admin (user_id, business_role)
 VALUES (6, 'Rol administrador');
+
+INSERT IGNORE INTO question
+VALUES (1, 'GOVERNANCE', 'Pregunta #1 Governance', 'TrueOrFalse', 10),
+       (2, 'GOVERNANCE', 'Pregunta #2 Governance', 'Ranking5', 20),
+       (3, 'ENVIRONMENTAL', 'Pregunta #1 Environmental', 'TrueOrFalse', 30),
+       (4, 'SOCIAL', 'Pregunta #1 Social', 'TrueOrFalse', 40);

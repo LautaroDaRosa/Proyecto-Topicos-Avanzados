f39efd78833162c6a69ec78044c2c80cc1aa1b7c
Santiago Villalba <santiago.villalba@koin.com.br>
2023-11-12 21:38:44 -0300

    fix

--- backend/src/main/java/los/trainees/backend/service/UserService.java
+++ backend/src/main/java/los/trainees/backend/service/UserService.java
@@ -50,8 +50,7 @@ public class UserService {
     public User checkCredentials(LoginRequest loginRequest) {
         Optional<User> optionalUser = findUserByUsername(loginRequest.getUsername());
         User user = optionalUser.orElseThrow(IncorrectUserDataException::new);
-        String hashedPassword = String.valueOf(loginRequest.getPassword().hashCode());
-        if (user.getPassword().compareTo(hashedPassword) == 0) {
+        if (user.getPassword().compareTo(loginRequest.getPassword()) == 0) {
             return user;
         }
         throw new IncorrectUserDataException();

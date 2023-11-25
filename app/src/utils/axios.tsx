0b651c65f17dfce02e84f9518be1bd94601a3d86
Gonzalo Martínez <martinezgonzalo6cel@gmail.com>
2023-11-25 01:02:45 -0300

    back url

--- app/src/utils/axios.tsx
+++ app/src/utils/axios.tsx
@@ -22,7 +22,7 @@ export const parseJwt = token => {
 
 // AXIOS INSTANCE
 export const axiosInstance = axios.create({
-  baseURL: `http://localhost:8080`,
+  baseURL: `https://topicos-avanzados-backend.azurewebsites.net`,
   headers: getHeaders(),
 });
 
